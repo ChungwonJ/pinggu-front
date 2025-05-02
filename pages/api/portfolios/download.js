5c5986927422e48304fe2bfd5b7f2b5bf852abd4
ChungWon <wjdcjddnjs1234@gmail.com>
2025-05-02 21:41:01 +0900

    파일다운수정

--- pages/api/portfolios/download.js
+++ pages/api/portfolios/download.js
@@ -3,24 +3,25 @@ import fetch from 'node-fetch';
 export default async function handler(req, res) {
   const { url } = req.query;
 
-  if (!url) {
-    return res.status(400).send('파일 URL이 제공되지 않았습니다.');
-  }
+  if (!url) return res.status(400).send('파일 URL이 제공되지 않았습니다.');
 
   try {
     const response = await fetch(decodeURIComponent(url));
 
-    if (!response.ok) {
-      throw new Error('S3에서 파일을 가져오는 데 실패했습니다.');
-    }
+    if (!response.ok) throw new Error('S3에서 파일을 가져오는 데 실패했습니다.');
 
     const contentType = response.headers.get('content-type') || 'application/octet-stream';
     const fileName = decodeURIComponent(url.split('/').pop());
 
-    res.setHeader('Content-Type', contentType);
-    res.setHeader('Content-Disposition', `attachment; filename="${fileName}"`);
+    const arrayBuffer = await response.arrayBuffer();
+    const buffer = Buffer.from(arrayBuffer);
 
-    response.body.pipe(res);
+    res.setHeader('Content-Type', contentType);
+    res.setHeader(
+      'Content-Disposition',
+      `attachment; filename*=UTF-8''${encodeURIComponent(fileName)}`
+    );
+    res.send(buffer);
   } catch (err) {
     console.error('파일 다운로드 실패:', err);
     res.status(500).send('파일 다운로드 중 오류가 발생했습니다.');
