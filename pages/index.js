e29236466e8375e5150f44f80ef46b18c5866043
ChungWon <wjdcjddnjs1234@gmail.com>
2025-05-03 02:36:14 +0900

    타이틀수정

--- pages/index.js
+++ pages/index.js
@@ -7,8 +7,8 @@ export default function Home() {
   return (
     <>
       <SeoHead
-        title="PortForU"
-        description="당신의 커리어를 위한 최고의 포트폴리오 플랫폼. IT 취업 정보까지 한 번에!"
+        title="PortForU | 포트포유 - IT 포트폴리오 공유 플랫폼"
+        description="포트포유는 IT 취업 준비생을 위한 최고의 포트폴리오 업로드 및 공유 플랫폼입니다."
         url="https://pinggu.vercel.app"
         image="./portforu.png"
       />
