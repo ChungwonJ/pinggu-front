7c37fc6573d8cb8df7740c3afde213a2befcd343
ChungWon <wjdcjddnjs1234@gmail.com>
2025-05-03 23:14:50 +0900

    ccccc

--- pages/api/subscribes/index.js
+++ pages/api/subscribes/index.js
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
           'Content-Type': 'application/json',
           ...(token && { Authorization: token }),
         },
-      });
+      });  
 
       const data = await response.json();
 
